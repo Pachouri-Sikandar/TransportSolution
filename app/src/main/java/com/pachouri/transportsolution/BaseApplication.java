068f56047cc28afb02c57681cb62a6ce0b291d12
Pachouri-Sikandar <ankitpachouri0891@gmail.com>
2016-12-24 18:56:24 +0530

    Added packages, base application and common util class

---
+++ app/src/main/java/com/pachouri/transportsolution/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.pachouri.transportsolution;
+
+import android.app.Application;
+
+/**
+ * Created by ankit on 12/24/16.
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/pachouri/transportsolution/util/CommonUtil.java
@@ -0,0 +1,7 @@
+package com.pachouri.transportsolution.util;
+
+/**
+ * Created by ankit on 12/24/16.
+ */
+public class CommonUtil {
+}
