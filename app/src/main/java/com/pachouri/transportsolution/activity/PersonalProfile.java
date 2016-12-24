d18ac90f51f50ad3ec24d21c3877de278bf87fa2
root <riyaz.a@tailoredtech.in>
2016-12-25 03:41:48 +0530

    git ignore

--- app/src/main/java/com/pachouri/transportsolution/activity/PersonalProfile.java
+++ app/src/main/java/com/pachouri/transportsolution/activity/PersonalProfile.java
@@ -2,18 +2,32 @@ package com.pachouri.transportsolution.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.pachouri.transportsolution.Constants;
 import com.pachouri.transportsolution.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class PersonalProfile extends AppCompatActivity {
 
     String mobilePhone;
 
+    @Bind(R.id.edt_first_name)
+    EditText editTextFirstName;
+
+    @Bind(R.id.edt_last_name)
+    EditText editTextLastName;
+
+    @Bind(R.id.edt_email)
+    EditText editTextEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_profile);
+        ButterKnife.bind(this);
         mobilePhone = getIntent().getStringExtra(Constants.PREF_KEY_PHONE_NUMBER);
     }
 }
