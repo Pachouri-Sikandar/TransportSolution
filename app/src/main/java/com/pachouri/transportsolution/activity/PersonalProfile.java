40cfbfb2d86d300b36fd6ab7bed6b9d6154bbfad
root <riyaz.a@tailoredtech.in>
2016-12-25 03:57:02 +0530

    saves personalprofile

--- app/src/main/java/com/pachouri/transportsolution/activity/PersonalProfile.java
+++ app/src/main/java/com/pachouri/transportsolution/activity/PersonalProfile.java
@@ -3,12 +3,13 @@ package com.pachouri.transportsolution.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
-
 import com.pachouri.transportsolution.Constants;
 import com.pachouri.transportsolution.R;
-
+import com.pachouri.transportsolution.models.UserModel;
+import com.pachouri.transportsolution.util.CommonUtil;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PersonalProfile extends AppCompatActivity {
 
@@ -30,4 +31,22 @@ public class PersonalProfile extends AppCompatActivity {
         ButterKnife.bind(this);
         mobilePhone = getIntent().getStringExtra(Constants.PREF_KEY_PHONE_NUMBER);
     }
+
+
+    @OnClick(R.id.btn_save)
+    protected void onClickSave(){
+        if(editTextFirstName.getText().toString().isEmpty())
+            MessageUtils.showToast(getApplicationContext(),"Invalid name");
+        else if(!CommonUtil.isValidEmail(editTextEmail.getText()))
+            MessageUtils.showToast(getApplicationContext(),"Invalid email");
+        else{
+            UserModel userModel = new UserModel();
+            userModel.setFirstName(editTextFirstName.getText().toString());
+            userModel.setLastName(editTextLastName.getText().toString());
+            userModel.setEmail(editTextEmail.getText().toString());
+            userModel.setMobileNumber(mobilePhone);
+            userModel.save();
+        }
+    }
+
 }
--- app/src/main/java/com/pachouri/transportsolution/util/CommonUtil.java
+++ app/src/main/java/com/pachouri/transportsolution/util/CommonUtil.java
@@ -18,4 +18,11 @@ public class CommonUtil {
         animator.start();
         /*"BackgroundColor"*/
     }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (target == null)
+            return false;
+
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
 }
