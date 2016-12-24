418229c14f43a07b3a26d55d21bbec40132ab0db
root <riyaz.a@tailoredtech.in>
2016-12-25 03:30:03 +0530

    personalProfile

--- app/src/main/java/com/pachouri/transportsolution/activity/PersonalProfile.java
+++ app/src/main/java/com/pachouri/transportsolution/activity/PersonalProfile.java
@@ -3,13 +3,17 @@ package com.pachouri.transportsolution.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pachouri.transportsolution.Constants;
 import com.pachouri.transportsolution.R;
 
 public class PersonalProfile extends AppCompatActivity {
+
+    String mobilePhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_profile);
+        mobilePhone = getIntent().getStringExtra(Constants.PREF_KEY_PHONE_NUMBER);
     }
 }
