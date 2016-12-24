138057113edb7a3a4dcc7a67bd51c9c96800551e
Pachouri-Sikandar <ankitpachouri0891@gmail.com>
2016-12-24 21:34:49 +0530

    added layout for home activity

--- app/src/main/java/com/pachouri/transportsolution/activity/HomeActivity.java
+++ app/src/main/java/com/pachouri/transportsolution/activity/HomeActivity.java
@@ -1,9 +1,18 @@
 package com.pachouri.transportsolution.activity;
 
+import android.os.Bundle;
+
 import com.pachouri.transportsolution.BaseActivity;
+import com.pachouri.transportsolution.R;
 
 /**
  * Created by ankit on 12/24/16.
  */
 public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 }
